4d3ab9e9504360955c270346393888ffec1ad235
123rafael123 <69690701r@gmail.com>
2025-09-16 19:36:37 -0400

    6to Cmmit creacion del modelo OrderItem

---
+++ ProyectoWeb2MvC/Models/OrderItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoWeb2MvC.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int IdProducto { get; set; }
+        public int cantidad { get; set; }
+        public decimal subtotal { get; set; }
+    }
+}
